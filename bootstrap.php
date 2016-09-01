d11e3dbf412749543e35d20952e46ed92ed152fa
janickiy <janickiy@mail.ru>
2016-09-01 13:50:52 +0300

    New changelist

--- bootstrap.php
+++ bootstrap.php
@@ -28,6 +28,7 @@ $cmspaths = array(
 	'tmp' => 'tmp'
 );
 
+
 require_once SYS_ROOT . $cmspaths['config'] . '/config_db.php';
 require_once SYS_ROOT . $cmspaths['core'] . '/core.php';
 core::init($cmspaths);
