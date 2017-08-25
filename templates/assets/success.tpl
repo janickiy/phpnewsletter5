c30bf68ae30edd4d976c30d0fdfeb9bda4496b11
Alexander Yanitsky <ayanitskiy@itmm.ru>
2017-08-25 23:47:51 +0300

    bug fix

---
+++ templates/assets/success.tpl
@@ -0,0 +1,6 @@
+<!-- IF '${MSG_ALERT}' != '' -->
+<div class="alert alert-success alert-dismissable">
+    <button class="close" aria-hidden="true" data-dismiss="alert" type="button">×</button>
+    ${MSG_ALERT}
+</div>
+<!-- END IF -->
---
+++ templates/js/ckeditor/plugins/imageuploader/info.php
@@ -0,0 +1,8 @@
+﻿<?php 
+
+if (isset($_FILES["fic"]["name"])) {
+	$path = isset($_POST['path']) ? urldecode($_POST['path']) : '';
+	if (move_uploaded_file($_FILES["fic"]["tmp_name"], $path . $_FILES["fic"]["name"])) {
+         echo "yes"; 
+	} else echo "no"; 
+} 
